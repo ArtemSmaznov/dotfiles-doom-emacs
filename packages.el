b8e6d954e5770871f1c6981134da180bc71dde08
Artem Smaznov <Artem.Smaznov@gmail.com>
2021-08-30 11:16:01 +0100

    Packages: adding qml mode

--- packages.el
+++ packages.el
@@ -34,6 +34,7 @@
 (package! feature-mode)
 (package! salt-mode)
 (package! groovy-mode)
+(package! qml-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
