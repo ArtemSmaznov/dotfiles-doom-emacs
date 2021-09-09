a9aff451d42fb9a413a1848d6967e5c7bc641c9f
Artem Smaznov <Artem.Smaznov@gmail.com>
2021-09-09 23:43:28 +0100

    LSP: adding support for viewing log files

--- packages.el
+++ packages.el
@@ -35,6 +35,7 @@
 (package! salt-mode)
 (package! groovy-mode)
 (package! qml-mode)
+(package! syslog-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
