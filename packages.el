52df53cc19c13cbed8fea9a09dc359226a57bb47
Artem Smaznov <ArtemSmaznov@gmail.com>
2022-06-11 11:33:01 -0400

    packages: org-auto-tangle

--- packages.el
+++ packages.el
@@ -36,6 +36,7 @@
 (package! groovy-mode)
 (package! qml-mode)
 (package! syslog-mode)
+(package! org-auto-tangle)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
