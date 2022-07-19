be5fa7971b207142ba36f3a1fbb8c0aa6c811905
Artem Smaznov <ArtemSmaznov@gmail.com>
2022-07-19 14:48:44 -0400

    init: enable nav-flash and rotate-text

--- init.el
+++ init.el
@@ -52,7 +52,7 @@
        (ligatures +extras)   ; ligatures and symbols to make your code pretty again
        minimap               ; show a map of the code on the side
        modeline              ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash           ; blink cursor line after big motions
+       nav-flash             ; blink cursor line after big motions
        ;; neotree            ; a project drawer, like NERDTree for vim
        ophints               ; highlight the region an operation acts on
        (popup +defaults)     ; tame sudden yet inevitable temporary windows
@@ -75,7 +75,7 @@
        ;;multiple-cursors    ; editing in many places at once
        ;;objed               ; text object editing for the innocent
        ;;parinfer            ; turn lisp into python, sort of
-       ;;rotate-text         ; cycle region at point between text candidates
+       rotate-text           ; cycle region at point between text candidates
        ;; snippets              ; my elves. They type so I don't have to
        ;;word-wrap           ; soft wrapping with language-aware indent
 
