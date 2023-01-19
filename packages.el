48782f370ef664230dd85b85f8b04c01716eb8a2
Artem Smaznov <ArtemSmaznov@gmail.com>
2023-01-18 19:40:41 -0500

    feat: add yuck-mode

--- packages.el
+++ packages.el
@@ -35,6 +35,7 @@
 (package! salt-mode)
 (package! syslog-mode)
 (package! vimrc-mode)
+(package! yuck-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
