515db9beb06ce95806a5a4cedef943e3ff75d497
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-02-14 11:43:23 -0500

    Revert "fix(org)!: call org-eldoc-load as a temporary fix"
    This reverts commit fbf4d1706176a425e1b6d57b2dd7db19e431712f.

--- packages.el
+++ packages.el
@@ -37,9 +37,6 @@
 (package! vimrc-mode)
 (package! yuck-mode)
 
-;; Unpinning packages
-;; (unpin! org)
-
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
