70e4d158cc0ba253059191f91b1e0557a8783824
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-07-14 18:57:29 -0400

    feat(lsp): enable qt

--- init.el
+++ init.el
@@ -184,7 +184,7 @@
        ;;plantuml                ; diagrams for confusing people more
        ;;purescript              ; javascript, but functional
        (python +lsp +pyright)    ; beautiful is better than ugly
-       ;;qt                      ; the 'cutest' gui framework ever
+       (qt +lsp)                      ; the 'cutest' gui framework ever
        ;;racket                  ; a DSL for DSLs
        ;;raku                    ; the artist formerly known as perl6
        ;;rest                    ; Emacs as a REST client
