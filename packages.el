6895587bf0ca3f66d9a8ef40f6580c82fb720e4b
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-12-28 20:07:21 -0500

    feat(ai): add default copilot setup

--- packages.el
+++ packages.el
@@ -43,6 +43,13 @@
 (package! vimrc-mode)
 (package! yuck-mode)
 
+;; ai
+(package! copilot
+  :recipe (:host github
+           :repo "copilot-emacs/copilot.el"
+           :files ("*.el")))
+
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
